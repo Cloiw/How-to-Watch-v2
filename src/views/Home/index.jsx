3a82dd8b1bd88312c74999040e75a902ea60f3a0
Cloi <sircli@hotmail.com>
2019-09-16 15:05:32 -0300

    agregando btn en home, modificando css navbar y home

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 import './Home.css';
 import imgDescription from '../../img/img_home.png';
 
@@ -9,19 +10,23 @@ class Home extends React.Component {
       <div className="home-container">
         <Container fluid>
           <section>
-          <Row bsPrefix="row row-description">
-            <Col md={7}>
-              <p className="home-description">
-                Our website helps you to watch your favorite movie sagas in the order that you want.<br></br>
-                You only need to select  you favorite saga and you'll find it ordered acording to its release date 
-                or by the order in which the story enlapses.
-              </p> 
-              <button> oli</button>
-            </Col>
-            <Col md={5}>
-              <img className="img-description" src={imgDescription} />
-            </Col>
-          </Row>
+            <Row bsPrefix="row row-description">
+              <Col md={5}>
+                <img className="img-description" src={imgDescription} />
+              </Col>
+              <Col md={7}>
+                <p className="home-description">
+                  This website helps you to watch your favorite movie sagas in the order that you want.<br></br>
+                  You only need to select a saga and you'll find it ordered acording to its release date 
+                  or by the order in which the story enlapses.
+                </p>
+                <LinkContainer to="/sagas">
+                  <Button href="" variant="start" size="lg" block>
+                    Start
+                  </Button>
+                </LinkContainer>
+              </Col> 
+            </Row>
           </section>
         </Container>
       </div>
