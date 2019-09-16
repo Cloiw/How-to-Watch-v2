15a07c70b7f74d3fafc961b4d1768c5fa59f953e
Cloi <sircli@hotmail.com>
2019-09-16 12:26:18 -0300

    borde navbar y prueba de view home

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-
+import { Container, Row, Col } from 'react-bootstrap'
 import './Home.css';
 
 class Home extends React.Component {
   render() {
     return (
-      <div className="user-container">
-      HOlis
+      <div className="home-container">
+        <Container>
+          <Row>
+          <Col md={{ span: 6, offset: 3 }}>
+          <p>sadksadjksa</p>
+          </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
