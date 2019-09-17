974d2f4ea2969218b004354177d0952e02b367c9
Cloi <sircli@hotmail.com>
2019-09-16 22:06:10 -0300

    correcciones timelines

--- src/views/TimeLines/index.jsx
+++ src/views/TimeLines/index.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import data from '../../data/data.json';
-import NoMatchPage from '../NoMatchPage/index.jsx';
+import NoMatchPage from '../NoMatchPage';
 
 const TimeLines = ({ match }) => (
   data[match.params.sagasId] === undefined ? <NoMatchPage />
     : (
       <div>
        soy esta saga men
-        {' '}
         {match.params.sagasId}
         {data[match.params.sagasId].map((x) => x.release)}
       </div>
     )
 );
+
+TimeLines.propTypes = {
+  match: PropTypes.objectOf(PropTypes.oneOfType([PropTypes.any])).isRequired,
+};
 
 export default TimeLines;
