7e77c8e86e320c015da57268b5cd872a09168813
Cloi <sircli@hotmail.com>
2019-09-24 13:08:20 -0300

    Updates

---
+++ precache-manifest.5947fe5d06171b9557afb44df1178730.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14dc3ee4bfac9fe2b8d7371922ee5b95",
+    "url": "/How-to-Watch-v2/index.html"
+  },
+  {
+    "revision": "edee86c5d2decb6c6ffa",
+    "url": "/How-to-Watch-v2/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "319456acd7005bfa43e7",
+    "url": "/How-to-Watch-v2/static/css/main.3bbd9c58.chunk.css"
+  },
+  {
+    "revision": "edee86c5d2decb6c6ffa",
+    "url": "/How-to-Watch-v2/static/js/2.782e7083.chunk.js"
+  },
+  {
+    "revision": "319456acd7005bfa43e7",
+    "url": "/How-to-Watch-v2/static/js/main.715d0f35.chunk.js"
+  },
+  {
+    "revision": "4c1614658fde79306a35",
+    "url": "/How-to-Watch-v2/static/js/runtime~main.8c020c87.js"
+  },
+  {
+    "revision": "8269800eca74976c3ba67969d47cb611",
+    "url": "/How-to-Watch-v2/static/media/alien_img.8269800e.png"
+  },
+  {
+    "revision": "c0a8ef32dc05b72174511372f1762537",
+    "url": "/How-to-Watch-v2/static/media/harry_potter_img.c0a8ef32.png"
+  },
+  {
+    "revision": "f3764bc208d0cb7861aa71d1b849ec80",
+    "url": "/How-to-Watch-v2/static/media/img_home.f3764bc2.png"
+  },
+  {
+    "revision": "15471d47d9e11e7f402103f557b1da19",
+    "url": "/How-to-Watch-v2/static/media/logo_crop.15471d47.png"
+  },
+  {
+    "revision": "bd3a386094bd5b6ed8aa187628b02498",
+    "url": "/How-to-Watch-v2/static/media/lord_of_the_rings_img.bd3a3860.png"
+  },
+  {
+    "revision": "d3daf2ba07b1609626195194aaa319a4",
+    "url": "/How-to-Watch-v2/static/media/marvel_img.d3daf2ba.png"
+  },
+  {
+    "revision": "a6ff7703f83452f0bd383f04d42e6a13",
+    "url": "/How-to-Watch-v2/static/media/pirates_of_the_caribbean_img.a6ff7703.png"
+  },
+  {
+    "revision": "e2c9923caa24b716541206cb1d99d26f",
+    "url": "/How-to-Watch-v2/static/media/saw_img.e2c9923c.png"
+  },
+  {
+    "revision": "0d2c093d0c0e20e8285509bb783203dd",
+    "url": "/How-to-Watch-v2/static/media/star_wars_img.0d2c093d.png"
+  },
+  {
+    "revision": "166e15e165033010ab666ded0a9af1c7",
+    "url": "/How-to-Watch-v2/static/media/startrek_img.166e15e1.png"
+  },
+  {
+    "revision": "b8d43b616ec8e422e0db241af8b3f19e",
+    "url": "/How-to-Watch-v2/static/media/the_conjuring_img.b8d43b61.png"
+  },
+  {
+    "revision": "eb54d002dfb3fbaf79142d4cd49c2b53",
+    "url": "/How-to-Watch-v2/static/media/xmen_img.eb54d002.png"
+  }
+]);
